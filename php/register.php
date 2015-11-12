0232d16510279480b5dbda8d4fcf43973de29906
Jordan Lord <jordanpaullord@hotmail.co.uk>
2015-11-12 11:20:49 +0000

    Update register.php, works with GET request

---
+++ php/register.php
@@ -0,0 +1,19 @@
+<?php
+
+require "database.php";
+
+
+$email = $_GET["email"];
+$first_name = $_GET["first_name"];
+$last_name = $_GET["last_name"];
+$password = $_GET["password"];
+
+$hash = password_hash($password, PASSWORD_BCRYPT);
+
+$query = $conn->prepare("INSERT INTO members (email, first_name, last_name, hash) VALUES (?, ?, ?, ?)");
+$query->bind_param("ssss", $email, $first_name, $last_name, $hash);
+$query->execute();
+
+die("success");
+
+?>
