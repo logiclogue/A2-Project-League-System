749c071df79ee445618054560fca1c73e9d85dbb
Jordan Lord <jordanpaullord@hotmail.co.uk>
2015-11-17 13:53:14 +0000

    Change database table 'members' to 'users'

--- php/Login.php
+++ php/Login.php
@@ -9,7 +9,7 @@ class Login
 		$email = $_GET['email'];
 		$password = $_GET['password'];
 
-		$query = Database::$conn->prepare("SELECT hash FROM members WHERE email=:email");
+		$query = Database::$conn->prepare("SELECT hash FROM users WHERE email=:email");
 		$query->bindParam(':email', $email, PDO::PARAM_STR);
 		$query->execute();
 
--- php/Register.php
+++ php/Register.php
@@ -13,7 +13,7 @@ class Register
 
 		$hash = password_hash($password, PASSWORD_BCRYPT);
 
-		$query = Database::$conn->prepare("INSERT INTO members (email, first_name, last_name, hash) VALUES (:email, :first_name, :last_name, :hash)");
+		$query = Database::$conn->prepare("INSERT INTO users (email, first_name, last_name, hash) VALUES (:email, :first_name, :last_name, :hash)");
 		$query->bindParam(':email', $email, PDO::PARAM_STR);
 		$query->bindParam(':first_name', $first_name, PDO::PARAM_STR);
 		$query->bindParam(':last_name', $last_name, PDO::PARAM_STR);
