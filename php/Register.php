8f5a0063d8f3da0b89630739566bcc375384ccb2
Jordan Lord <jordanpaullord@hotmail.co.uk>
2015-11-17 13:48:53 +0000

    Fix Register class

--- php/Register.php
+++ php/Register.php
@@ -13,11 +13,18 @@ class Register
 
 		$hash = password_hash($password, PASSWORD_BCRYPT);
 
-		$query = $conn->prepare("INSERT INTO members (email, first_name, last_name, hash) VALUES (?, ?, ?, ?)");
-		$query->bind_param("ssss", $email, $first_name, $last_name, $hash);
-		$query->execute();
-
-		die("success");
+		$query = Database::$conn->prepare("INSERT INTO members (email, first_name, last_name, hash) VALUES (:email, :first_name, :last_name, :hash)");
+		$query->bindParam(":email", $email, PDO::PARAM_STR);
+		$query->bindParam(":first_name", $first_name, PDO::PARAM_STR);
+		$query->bindParam(":last_name", $last_name, PDO::PARAM_STR);
+		$query->bindParam(":hash", $hash, PDO::PARAM_STR);
+		
+		if ($query->execute()) {
+			die("Success");
+		}
+		else {
+			die("Failure to create user");
+		}
 	}
 }
 
