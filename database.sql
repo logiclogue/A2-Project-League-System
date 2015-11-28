68f6c53117412b3bee4071e8802893e5db4af4a2
logiclogue <jordanpaullord@hotmail.co.uk>
2015-11-28 11:53:51 +0000

    Fix database.sql

--- database.sql
+++ database.sql
@@ -1,11 +1,3 @@
-n SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Nov 28, 2015 at 11:48 AM
--- Server version: 5.5.46-0ubuntu0.14.04.2
--- PHP Version: 5.5.9-1ubuntu4.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
