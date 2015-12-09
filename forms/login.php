06a9f5ea04cef786fafb9380ad0c6140a8b84fcf
logiclogue <jordanpaullord@hotmail.co.uk>
2015-12-09 19:34:21 +0000

    Create profile and login forms

---
+++ forms/login.php
@@ -0,0 +1,66 @@
+<div class="container">
+
+	<!-- Login form -->
+	<form>
+		<div class="row box-container six columns">
+			<h1>Login</h1>
+
+			<!-- Email address field -->
+			<div class="u-full-width">
+				<label>Email Address</label>
+				<input class="u-full-width" type="email">
+			</div>
+
+			<!-- Password field -->
+			<div class="u-full-width">
+				<label>Password</label>
+				<input class="u-full-width" type="password">
+			</div>
+
+			<!-- Login button -->
+			<button class="u-full-width">Login</button>
+
+		</div>
+	</form>
+
+	<!-- Register form -->
+	<form>
+		<div class="row box-container six columns u-pull-right">
+			<h1>Register</h1>
+
+			<!-- First name field -->
+			<div class="u-full-width">
+				<label>First Name</label>
+				<input class="u-full-width" type="text">
+			</div>
+
+			<!-- Last name field -->
+			<div class="u-full-width">
+				<label>Last Name</label>
+				<input class="u-full-width" type="text">
+			</div>
+
+			<!-- Email address field -->
+			<div class="u-full-width">
+				<label>Email Address</label>
+				<input class="u-full-width" type="email">
+			</div>
+
+			<!-- Password field -->
+			<div class="u-full-width">
+				<label>Password</label>
+				<input class="u-full-width" type="password">
+			</div>
+
+			<!-- Repeat password field -->
+			<div class="u-full-width">
+				<label>Repeat Password</label>
+				<input class="u-full-width" type="password">
+			</div>
+
+			<!-- Register button -->
+			<button class="u-full-width">Register</button>
+
+		</div>
+	</form>
+</div>
---
+++ forms/profile.php
@@ -0,0 +1,26 @@
+<div class="container">
+
+	<div class="box-container seven columns">
+		<h1>Jordan Lord</h1>
+
+		<div class="result">
+			<span>TUE 03 NOV 2015</span>
+			<span>Won</span>
+			<span>vs <a>John Doe</a></span>
+			<span>3-1</span>
+			<span>Premier League</span>
+		</div>
+	</div>
+
+	<div class="five columns">
+		<div class="box-container">
+			<h3>Rating</h3>
+			<span>1342</span>
+		</div>
+
+		<div class="box-container">
+			<h2>Contact</h2>
+		</div>
+	</div>
+
+</div>
