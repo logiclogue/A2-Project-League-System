b783c375b3699c0fb9cf66668b59375bf8bb071c
logiclogue <jordanpaullord@hotmail.co.uk>
2015-12-11 09:54:49 +0000

    Add tournament form

---
+++ forms/tournament.php
@@ -0,0 +1,48 @@
+<div class="container">
+
+	<div class="box-container seven columns">
+		<h1>Premier League</h1>
+
+		<table class="u-full-width">
+			<thead>
+				<tr>
+					<th>Pos</th>
+					<th>Name</th>
+					<th>Wins</th>
+					<th>Losses</th>
+					<th>Rating</th>
+					<th>Pts</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>1</td>
+					<td>Jordan Lord</td>
+					<td>1</td>
+					<td>1</td>
+					<td>1342</td>
+					<td>8</td>
+				</tr>
+
+				<tr>
+					<td>2</td>
+					<td>John Doe</td>
+					<td>1</td>
+					<td>1</td>
+					<td>1312</td>
+					<td>6</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+
+	<div class="five columns">
+		<div class="box-container side">
+			<h2>Members<a class="u-pull-right">+</a></h2>
+
+			<span><a>Jordan Lord</a></span><br>
+			<span><a>John Doe</a></span>
+		</div>
+	</div>
+
+</div>
