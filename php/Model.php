5a22fa1db48505ddcc190f3dc487642957837592
logiclogue <jordanpaullord@hotmail.co.uk>
2015-12-21 14:46:53 +0000

    Add Model class

---
+++ php/Model.php
@@ -0,0 +1,23 @@
+<?php
+
+class Model
+{
+	protected static $data = array();
+
+
+	public static function call($data) {
+		self::$data = $data;
+
+		return static::main();
+	}
+
+	public static function init() {
+		if (GetJSON::isPost()) {
+			self::$data = GetJSON::decodePost();
+
+			echo json_encode(static::main());
+		}
+	}
+}
+
+?>
