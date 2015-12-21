8b4a48b15816df6c7dfbf2b5c93aaaba14686a5e
logiclogue <jordanpaullord@hotmail.co.uk>
2015-12-21 14:43:16 +0000

    Update Database class

--- php/Database.php
+++ php/Database.php
@@ -5,17 +5,24 @@ class Database
 	public static $conn;
 
 
+	public static function create() {
+		$query = file_get_contents(dirname(__DIR__) . '/database.sql');
+		$result = self::$conn->prepare($query);
+
+		if ($result->execute()) {
+			echo 'Success';
+		}
+		else {
+			echo 'Failure';
+		}
+	}
+
 	public static function init() {
 		$string = file_get_contents(dirname(__DIR__) . '/env.json');
 		$data = json_decode($string, true);
 		$connData = $data['database'];
 
 		self::$conn = new PDO('mysql:host=' . $connData['servername'] . ';dbname=' . $connData['database'], $connData['username'], $connData['password']);
-
-		// must error check here!
-
-		// start session
-		session_start();
 	}
 }
 
