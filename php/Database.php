3366471f4cb048c35aa971091a235551d2d740d4
logiclogue <jordanpaullord@hotmail.co.uk>
2015-12-22 10:33:27 +0000

    Comment Model class

--- php/Database.php
+++ php/Database.php
@@ -1,10 +1,28 @@
 <?php
 
+/**
+ * The Database class connects the the database when the file is included.
+ *
+ * @class Database
+ * @static
+ */
 class Database
 {
+	/**
+	 * This variable is the connection variable for connecting to the database.
+	 *
+	 * @property conn
+	 * @type Object
+	 */
 	public static $conn;
 
 
+	/**
+	 * This method is used to create the tables and columns in the database.
+	 * It `database.sql` file provides the SQL to do this.
+	 *
+	 * @method create
+	 */
 	public static function create() {
 		$query = file_get_contents(dirname(__DIR__) . '/database.sql');
 		$result = self::$conn->prepare($query);
@@ -17,6 +35,13 @@ class Database
 		}
 	}
 
+	/**
+	 * This method is called when the file is included.
+	 * It is used to connect to the database.
+	 * Also to set @property conn to new PDO.
+	 *
+	 * @method init
+	 */
 	public static function init() {
 		$string = file_get_contents(dirname(__DIR__) . '/env.json');
 		$data = json_decode($string, true);
@@ -26,6 +51,8 @@ class Database
 	}
 }
 
+
+// @method init called straight away.
 Database::init();
 
 ?>
--- php/Model.php
+++ php/Model.php
@@ -1,19 +1,76 @@
 <?php
 
+/**
+ * Model class, the class is extended by all models.
+ * It provides a foundation for all models.
+ *
+ * @class Model
+ * @static
+ */
+
 class Model
 {
+	/**
+	 * The name of the data variable in POST.
+	 *
+	 * @property name
+	 * @private
+	 */
+	private static $name = 'JSON';
+
+	/**
+	 * The data object is used to store.
+	 *
+	 * @property data
+	 * @type Object
+	 */
 	protected static $data = array();
 
 
+	/**
+	 * This method is used to decode the JSON data in the post.
+	 *
+	 * @method
+	 * @return {Object} The decoded JSON.
+	 */
+	private static function decodePost() {
+		if (isset($_POST[self::$name])) {
+			return json_decode($_POST[self::$name], true);
+		}
+	}
+
+	/**
+	 * This returns whether the post data variable is set.
+	 *
+	 * @method isPost
+	 * @return {Boolean} Whether it's set.
+	 */
+	private static function isPost() {
+		return isset($_POST[self::$name]);
+	}
+
+
+	/**
+	 * Call allows PHP to pass data into the model.
+	 *
+	 * @method call
+	 * @param {Object} Data object to interact with the model.
+	 * @return {} The return of child @method main.
+	 */
 	public static function call($data) {
 		self::$data = $data;
 
 		return static::main();
 	}
 
+	/**
+	 * Function that is called to check if it is called with Post.
+	 *
+	 * @method init
+	 */
 	public static function init() {
-		if (GetJSON::isPost()) {
-			self::$data = GetJSON::decodePost();
+		if (self::isPost()) {
+			self::$data = self::decodePost();
 
 			echo json_encode(static::main());
 		}
