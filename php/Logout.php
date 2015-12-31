335286fc55e658ae94b50ac7a376879cd69662c3
logiclogue <jordanpaullord@hotmail.co.uk>
2015-12-31 11:48:16 +0000

    Add Logout model

---
+++ php/Logout.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once(dirname(__DIR__) . '/php/Model.php');
+
+session_start();
+
+
+/**
+ * Logout model called when the user logs out.
+ *
+ * @class Logout
+ * @extends Model
+ * @static
+ */
+class Logout extends Model
+{
+	/**
+	 * Method that destroys the session.
+	 *
+	 * @method main
+	 * @protected
+	 * @return {Boolean} True.
+	 */
+	protected static function main() {
+		session_unset();
+		session_destroy();
+
+		return true;
+	}
+}
+
+Logout::init();
+
+?>
