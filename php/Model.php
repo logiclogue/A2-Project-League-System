499809b081f8a39276d1e85382db6345cc48ebb3
logiclogue <jordanpaullord@hotmail.co.uk>
2016-01-14 07:46:50 +0000

    Update @class Model

--- php/Model.php
+++ php/Model.php
@@ -21,15 +21,60 @@ class Model
 	 * The data object is used to store.
 	 *
 	 * @property data
-	 * @type Object
+	 * @type Array
+	 * @protected
 	 */
 	protected static $data = array();
 
+	/**
+	 * The object for holding all data that wants to be returned.
+	 *
+	 * @property return_data
+	 * @type Array
+	 * @protected
+	 */
+	protected static $return_data = array();
+
+	/**
+	 * Whether model executed successfully.
+	 *
+	 * @property success
+	 * @type Boolean
+	 * @protected
+	 */
+	protected static $success = true;
+
+	/**
+	 * Error message string, if error.
+	 *
+	 * @property error_msg
+	 * @type String
+	 * @protected
+	 */
+	protected static $error_msg = '';
+
+
+	/**
+	 * Method that assembles the return object.
+	 *
+	 * @method returnObj
+	 * @private
+	 * @return {Array} Return object.
+	 */
+	private static function returnObj() {
+		 $obj = self::$return_data;
+
+		 $obj['success'] = self::$success;
+		 $obj['error_msg'] = self::$error_msg;
+
+		 return $obj;
+	}
 
 	/**
 	 * This method is used to decode the JSON data in the post.
 	 *
 	 * @method
+	 * @private
 	 * @return {Object} The decoded JSON.
 	 */
 	private static function decodePost() {
@@ -42,6 +87,7 @@ class Model
 	 * This returns whether the post data variable is set.
 	 *
 	 * @method isPost
+	 * @private
 	 * @return {Boolean} Whether it's set.
 	 */
 	private static function isPost() {
@@ -53,25 +99,31 @@ class Model
 	 * Call allows PHP to pass data into the model.
 	 *
 	 * @method call
+	 * @public
 	 * @param {Object} Data object to interact with the model.
 	 * @return {} The return of child @method main.
 	 */
 	public static function call($data) {
 		self::$data = $data;
 
-		return static::main();
+		static::main();
+
+		return self::returnObj();
 	}
 
 	/**
 	 * Function that is called to check if it is called with Post.
 	 *
 	 * @method init
+	 * @public
 	 */
 	public static function init() {
 		if (self::isPost()) {
 			self::$data = self::decodePost();
 
-			echo json_encode(static::main());
+			static::main();
+
+			echo json_encode(self::returnObj());
 		}
 	}
 }
