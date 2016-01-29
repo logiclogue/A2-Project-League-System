076541613224ab4c769e4af5bdeb30444c6e0b29
logiclogue <jordanpaullord@hotmail.co.uk>
2016-01-29 18:28:07 +0000

    Add @directive cpResult

---
+++ directives/cpResult.js
@@ -0,0 +1,11 @@
+/**
+ * Directive for handling results.
+ *
+ * @directive cpResult
+ */
+app.directive('cpResult', function ()
+{
+	return {
+		templateUrl: 'views/results.html'
+	}
+});
--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 	<script src="app/app.js"></script>
 	<script src="app/config.js"></script>
 	<script src="services/CallModel.js"></script>
+	<script src="directives/cpResult.js"></script>
 	<script src="controllers/LoginCtrl.js"></script>
 	<script src="controllers/ProfileCtrl.js"></script>
 	<script src="controllers/NavCtrl.js"></script>
