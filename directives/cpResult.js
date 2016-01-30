b1bebc1b9a4387382d92653be50833443ba0bf53
logiclogue <jordanpaullord@hotmail.co.uk>
2016-01-30 22:30:59 +0000

    Update @directive cpResult, gets some result data from server

--- directives/cpResult.js
+++ directives/cpResult.js
@@ -3,9 +3,50 @@
  *
  * @directive cpResult
  */
-app.directive('cpResult', function ()
+app.directive('cpResult', function (CallModel)
 {
+	var self,
+		days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],
+		months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+
+
+	function getDateString(dateString) {
+		var date = new Date(dateString);
+
+		return days[date.getDay()] + ' ' + date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear();
+	}
+
+	function sortResults() {
+		self.results.forEach(function (result) {
+			if (result.score1 > result.score2) {
+				result.winOrLoss = 'win';
+			}
+			else {
+				result.winOrLoss = 'loss';
+			}
+
+			result.date = getDateString(result.date);
+		});
+	}
+
+	function getResults() {
+		CallModel.fetch('ResultGet', {},
+		{
+			success: function (response) {
+				self.results = response.results;
+
+				sortResults();
+			}
+		});
+	}
+
+
 	return {
-		templateUrl: 'views/results.html'
+		templateUrl: 'views/results.html',
+		link: function (scope) {
+			self = scope;
+
+			getResults();
+		}
 	}
 });
