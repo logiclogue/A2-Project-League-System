62d971f541f28b2b3bd651259a30395cb4be635d
logiclogue <jordanpaullord@hotmail.co.uk>
2016-02-04 13:08:48 +0000

    Add @class FixturesGet, no functionality yet

---
+++ models/FixturesGet.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once(dirname(__DIR__) . '/php/Model.php');
+require_once(dirname(__DIR__) . '/php/Database.php');
+
+
+/**
+ * Model that returns fixture list for a tournament or user.
+ *
+ * @class FixturesGet
+ * @extends Model
+ */
+class FixturesGet extends Model
+{
+	/**
+	 *
+	 *
+	 *
+	 */
+	protected function main() {
+
+	}
+}
+
+?>
