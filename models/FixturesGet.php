1d3e134c922e1efc3c76d845aeb38d0648550335
logiclogue <jordanpaullord@hotmail.co.uk>
2016-02-05 21:37:53 +0000

    Update @class FixturesGet, works on query string

--- models/FixturesGet.php
+++ models/FixturesGet.php
@@ -10,6 +10,10 @@ require_once(dirname(__DIR__) . '/php/Database.php');
  * @class FixturesGet
  * @extends Model
  */
+/**
+ *
+ *
+ */
 class FixturesGet extends Model
 {
 	/**
@@ -17,8 +21,35 @@ class FixturesGet extends Model
 	 *
 	 *
 	 */
-	protected function main() {
+	private $query = <<<SQL
+		SELECT
+		u.id,
+		CONCAT(u.first_name, ' ', u.last_name) name,
+		tu.is_player,
+		tu.tournament_id
+		FROM users u
+		INNER JOIN tournament_user_maps tu
+		ON tu.user_id = u.id
+		WHERE
+		tu.tournament_id = 1 AND
+		tu.is_player = TRUE AND
+		u.id <> :user_id
+SQL;
 
+
+	/**
+	 *
+	 *
+	 *
+	 */
+	protected function main() {
+		$stmt = Database::$conn->prepare($this->query);
+
+		$stmt->bindParam(':user_id', $this->data['user_id']);
+
+		if ($stmt->execute()) {
+			
+		}
 	}
 }
 
