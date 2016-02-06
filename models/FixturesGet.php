ba5719586f1ba8993fb0c2c4fd3e2df74f051e85
logiclogue <jordanpaullord@hotmail.co.uk>
2016-02-06 08:58:16 +0000

    Add SQL query string for getting fixtures, @class FixturesGet

--- models/FixturesGet.php
+++ models/FixturesGet.php
@@ -23,17 +23,27 @@ class FixturesGet extends Model
 	 */
 	private $query = <<<SQL
 		SELECT
-		u.id,
-		CONCAT(u.first_name, ' ', u.last_name) name,
-		tu.is_player,
-		tu.tournament_id
-		FROM users u
+		u1.id player1_id,
+		u2.id player2_id,
+		CONCAT(u1.first_name, ' ', u1.last_name) player1_name,
+		CONCAT(u2.first_name, ' ', u2.last_name) player2_name,
+		tu.tournament_id t_id_1,
+		tu2.tournament_id t_id_2
+		FROM users u1
+
+		INNER JOIN users u2
 		INNER JOIN tournament_user_maps tu
-		ON tu.user_id = u.id
+		ON tu.user_id = u1.id
+		INNER JOIN tournament_user_maps tu2
+		ON tu2.user_id = u2.id
+
 		WHERE
-		tu.tournament_id = 1 AND
+		tu.tournament_id = tu2.tournament_id AND
 		tu.is_player = TRUE AND
-		u.id <> :user_id
+		tu2.is_player = TRUE AND
+		u1.id <> u2.id AND
+		CASE WHEN :tournament_id IS NULL THEN TRUE ELSE tu.tournament_id = :tournament_id END
+		CASE WHEN :user_id IS NULL THEN u1.id > u2.id ELSE u1.id = :user_id END
 SQL;
 
 
