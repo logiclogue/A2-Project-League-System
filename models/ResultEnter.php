8e0a64584d6ae0c8b5577dac10e319088a1fcd3d
logiclogue <jordanpaullord@hotmail.co.uk>
2016-02-08 21:33:17 +0000

    Fix results against self being entered

--- models/ResultEnter.php
+++ models/ResultEnter.php
@@ -73,18 +73,20 @@ SQL;
 		else if (!$this->isLeagueManager($this->data['player1_id'], $this->data['tournament_id'])) {
 			$this->error_msg = "You don't have permission to do that";
 		}
+		
 		// Check if opponent is in league.
-		else if (!$this->isPlayer($this->data['player2_id'], $this->data['tournament_id'])) {
+		if (!$this->isPlayer($this->data['player2_id'], $this->data['tournament_id'])) {
 			$this->error_msg = "Opponent not in the league";
 
 			return false;
 		}
 		// Check if opponent is not self.
-		else if ($this->data['player1_id'] == $this->data['player2_id']) {
+		if ($this->data['player1_id'] == $this->data['player2_id']) {
 			$this->error_msg = "You can't enter a result against yourself";
 
 			return false;
 		}
+		
 		// Check if result already exists.
 		// Check if score is valid.
 
